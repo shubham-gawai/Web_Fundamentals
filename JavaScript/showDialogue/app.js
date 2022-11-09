e72353e18ab170bf130def9d385e37a1aa958837
Shubham Gawai <gawaishubham001@gmail.com>
2022-11-09 10:00:06 +0530

    task updated

---
+++ JavaScript/showDialogue/app.js
@@ -0,0 +1,17 @@
+//  on mouseover the button should open a dialogue box:
+
+  
+function showdialogue(x) { 
+    x.style.backgroundColor="green";
+    // var a = document.getElementByClassName("new-div1");
+    
+    // var c = document.createElement('div');
+    // a.appendChild(c).display;
+    // document.getElementByClassName("new-div1").appendChild(a);
+}
+ 
+function hidedialogue(x){
+    x.style.backgroundColor="orangered";
+}
+
+ 
