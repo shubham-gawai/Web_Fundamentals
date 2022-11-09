f8cb0b897b1e508eb2099297e0f12fa6174c8e1b
Shubham Gawai <gawaishubham001@gmail.com>
2022-11-09 11:04:07 +0530

    updated

--- Bootstrap/marketing_website/app.js
+++ Bootstrap/marketing_website/app.js
@@ -1,26 +1,26 @@
 // Header Scroll
 
-let nav = document.querySelector('.navbar');
+let nav = document.querySelector(".navbar");
 window.onscroll = function () {
   if (document.documentElement.scrollTop > 20) {
-    nav.classList.add('header-scrolled');
+    nav.classList.add("header-scrolled");
   } else {
-    nav.classList.remove('header-scrolled');
+    nav.classList.remove("header-scrolled");
   }
 };
 
 // nav hide
-let navBar = document.querySelectorAll('.nav-link');
-let navCollapse = document.querySelector('.navbar-collapse.collapse');
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
 navBar.forEach(function (a) {
-  a.addEventListener('click', function () {
-    navCollapse.classList.remove('show');
+  a.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
   });
 });
 
 // Owl Carousel
 $(document).ready(function () {
-  $('.client-slider-section').owlCarousel({
+  $(".client-slider-section").owlCarousel({
     items: 4,
     loop: true,
     nav: false,
